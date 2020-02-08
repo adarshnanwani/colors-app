41a1fed57d214beedcdadcea80222c08598a4553
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-08 19:07:02 +0530

    Add palette footer.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import Slider from "rc-slider";
 import { Select, MenuItem } from "@material-ui/core";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
   state = {
-    format: "hex"
+    format: "hex",
+    open: false
   };
 
   handleChange = e => {
-    this.setState({ format: e.target.value });
+    this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
+  };
+
+  closeSnackbar = () => {
+    this.setState({ open: false });
   };
   render() {
     const { level, changeLevel } = this.props;
-    const { format } = this.state;
+    const { format, open } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -40,6 +48,26 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          onClose={this.closeSnackbar}
+          autoHideDuration={3000}
+          message={<span id="message-id">Format Changed to {format.toUpperCase()}!</span>}
+          ContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
