66fca86e52a11e086415b1466021af455aa29519
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-08 17:27:54 +0530

    Add Navbar and Roboto font.

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import Slider from 'rc-slider';
-import ColorBox from './ColorBox';
-import "rc-slider/assets/index.css";
-import './Palette.css';
+import Navbar from "./Navbar";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
 class Palette extends Component {
   state = {
@@ -10,28 +9,18 @@ class Palette extends Component {
   };
 
   changeLevel = level => {
-    this.setState({level});
-  }
+    this.setState({ level });
+  };
   render() {
-    const {colors} = this.props.palette;
-    const {level} = this.state;
-    const colorBoxes = colors[level].map(
-      color => {
-        return <ColorBox background={color.hex} name={color.name} />;
-      }
-    );
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map(color => {
+      return <ColorBox background={color.hex} name={color.name} />;
+    });
     return (
       <div className="Palette">
         {/* Navbar goes here */}
-        <div className="slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className="Palette-colors">
           {/* Bunch of color boxes */}
           {colorBoxes}
